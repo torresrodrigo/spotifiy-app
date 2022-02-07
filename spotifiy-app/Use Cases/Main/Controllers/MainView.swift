53d3cef6bfa5d737ba955408576b91c3d0d8abf0
Rodrigo Tores <rtorres@munidigital.com>
2022-02-06 23:03:30 -0300

    Setup folders for MainView

---
+++ spotifiy-app/Use Cases/Main/Controllers/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  MainView.swift
+//  spotifiy-app
+//
+//  Created by Rodrigo Torres on 06/02/2022.
+//
+
+import UIKit
+
+class MainView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
