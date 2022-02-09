0ed66cb1be2f8a52ffa991a2436042b05ca2746d
Rodrigo Tores <rtorres@munidigital.com>
2022-02-08 23:32:33 -0300

    Add logic in func to call api

---
+++ spotifiy-app/Services/APIManager.swift
@@ -0,0 +1,34 @@
+//
+//  APIManager.swift
+//  spotifiy-app
+//
+//  Created by Rodrigo Torres on 08/02/2022.
+//
+
+import Foundation
+import RxSwift
+import Alamofire
+
+class APIManager {
+    
+    static let shared = APIManager()
+    
+    func getArtist(params: [String: String])  -> Observable<Artists> {
+        let url = Constants.SEARCH_ENDPOINT
+        let headers = Constants.headers
+        
+        return Observable.create { observer in
+            let requestReference = AF.request(url, method: .get, parameters: params, headers: headers).responseDecodable(of: DataResponse.self) { response in
+                switch response.result {
+                case .success(let data):
+                    observer.onNext(data.artists)
+                case .failure(let error):
+                    observer.onError(error.underlyingError!)
+                }
+            }
+            return Disposables.create {
+                requestReference.cancel()
+            }
+        }
+    }
+}
